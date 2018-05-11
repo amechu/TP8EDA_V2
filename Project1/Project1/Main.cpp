72fda9fbf398654e6d713e7b3f65a67a6f434a24
Alan <31071359+amechu@users.noreply.github.com>
2018-05-11 18:55:09 -0300

    asfasf

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -7,41 +7,42 @@ int main(int argc, char*argv[]) {
 	Parser parser;
 	Menu mainMenu;
 
-	mainMenu.setParserError(parser.Read(argc, argv));
-	mainMenu.setFilesystemError(fileReader.load(parser.getPath()));
-	mainMenu.setImageError(mainMenu.loadImages());
-	mainMenu.setENCDError(mainMenu.loadENCD());
+	mainMenu.setParserError(parser.Read(argc, argv));					//Se fija si hubo
+	mainMenu.setFilesystemError(fileReader.load(parser.getPath()));		//algun error a lo largo del
+	mainMenu.setImageError(mainMenu.loadImages());						//seteo inicial del programa
+	mainMenu.setENCDError(mainMenu.loadENCD());							//y si lo hay, lo carga
 
-	mainMenu.checkError();
+	mainMenu.checkError();												//Se fija que errores hubo y cambia el estado y variable de error acorde
 
-	if (mainMenu.getError() != menuError::NO_ERROR) {
-		mainMenu.reportError();
+	if (mainMenu.getError() != menuError::NO_ERROR) {					//Si hubo algun error
+		mainMenu.reportError();											//lo reporto al usuario y cierro programa
 	}
 	else {
-		mainMenu.setState(menuState::MAINMENU);
-		mainMenu.loopMenu();
+		mainMenu.setState(menuState::MAINMENU);							//Sino, comienzo mainmenu, espero que el usuario diga si quiere comprimir
+		mainMenu.loopMenu();											//o decomprimir. Siempre haciendo un notify() por cada cambio.
 		if (mainMenu.getState() == menuState::ENCODER) {
-			mainMenu.notify();
-			if (mainMenu.encodeSelected()) {
-				mainMenu.setState == menuState::SUCCESS;
-				mainMenu.notify();
+			mainMenu.loopEncoder();										//Si quizo comprimir, entro en menu de comprimir
+			if (mainMenu.encode()) {									//Comprimo
+				mainMenu.setState == menuState::SUCCESS;				//Si fue exitoso
+				mainMenu.notify();										//Notifico exito
 			}
 			else {
-				mainMenu.setState == menuState::ERROR;
-				mainMenu.notify();
+				mainMenu.setState == menuState::ERROR;					//Si hubo error
+				mainMenu.notify();										//Notifico error
 			}
 		}
-		else if (mainMenu.getState() == menuState::DECODER) {
-			if (mainMenu.decodeSelected()) {
-				mainMenu.setState == menuState::SUCCESS;
-				mainMenu.notify();
+		else if (mainMenu.getState() == menuState::DECODER) {			//Si quizo decomprimir, entro en menu de decomprimir
+			mainMenu.loopDecoder();										
+			if (mainMenu.decode()) {									//Decomprimo
+				mainMenu.setState == menuState::SUCCESS;				//Si fue exitoso
+				mainMenu.notify();										//Notifico exito
 			}
 			else {
-				mainMenu.setState == menuState::ERROR;
-				mainMenu.notify();
+				mainMenu.setState == menuState::ERROR;					//Si hubo error
+				mainMenu.notify();										//Notifico error
 			}
 		}
 	}
-
-	return EXIT_SUCCESS;
+		
+	return EXIT_SUCCESS;												//Salgo del programa
 }
